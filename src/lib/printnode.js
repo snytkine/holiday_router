148a1236ef56732a9ce1ee558c2d57387f4f3a3a
Dmitri <dmitri.snytkine@adp.com>
2019-12-18 21:46:49 -0500

    Updated packages.json to setup code coverage

--- src/lib/printnode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("./index");
-exports.printNode = function (node, indent) {
-    if (indent === void 0) { indent = 0; }
-    return "\n    " + ' '.repeat(indent * 4) + " || " + '='.repeat(36) + "\n    " + ' '.repeat(indent * 4) + " || node=" + node.name + "\n    " + ' '.repeat(indent * 4) + " || priority=" + node.priority + "\n    " + ' '.repeat(indent * 4) + " || hasControllers=" + (node.controllers.length > 0) + "\n    " + ' '.repeat(indent * 4) + " || children (" + node.children.length + ") =" + index_1.printChildren(node.children, indent) + "\n    " + ' '.repeat(indent * 4) + " || " + '='.repeat(36);
-};
