8ded62021a9c23fc6d48a588e8f06466ed61cfc6
Dmitri <dmitri.snytkine@adp.com>
2019-12-22 18:32:07 -0500

    Added unit tests for pathparamnoderegex

--- src/tests/nodes/pathparamnoderegex.test.ts
+++ src/tests/nodes/pathparamnoderegex.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import {
+  PathParamNode,
   PathParamNodeRegex,
   PRIORITY
 } from '../../nodes'
@@ -7,6 +8,7 @@ import {
   RouterError,
   RouterErrorCode
 } from '../../errors'
+import { BasicController } from '../../lib'
 
 describe('#pathparamnoderegex node', () => {
   describe('#PathParamNode object test', () => {
@@ -75,5 +77,80 @@ describe('#pathparamnoderegex node', () => {
         expect(e.code).to.be.equal(RouterErrorCode.MAKE_URI_REGEX_FAIL)
       }
     })
+
+    it('.findRoutes should return iterator with all matches', () => {
+      const nodeWithPrefixAndPostfix = new PathParamNodeRegex('id', new RegExp('([0-9]+)'), '.html', 'order-');
+      const ctrl = new BasicController('controller1', 'id1', 2)
+      const ctrl2 = new BasicController('controller2', 'id2', 1)
+      nodeWithPrefixAndPostfix.addController(ctrl);
+      nodeWithPrefixAndPostfix.addController(ctrl2);
+
+      const foundRoutes = nodeWithPrefixAndPostfix.findRoutes('order-1234.html');
+      const route1 = foundRoutes.next();
+      const route2 = foundRoutes.next();
+      expect(route1.value.node)
+      .to
+      .equal(nodeWithPrefixAndPostfix)
+
+      expect(route1.value.controller)
+      .to
+      .equal(ctrl)
+
+      expect(route1.value.params.pathParams[0].paramName)
+      .to
+      .equal('id')
+
+      expect(route1.value.params.pathParams[0].paramValue)
+      .to
+      .equal('1234')
+
+      expect(route2.value.node)
+      .to
+      .equal(nodeWithPrefixAndPostfix)
+
+      expect(route2.value.controller)
+      .to
+      .equal(ctrl2)
+    })
+
+
+
+    it('PathParamNodeRegex with child nodes .findRoutes should return iterator from child nodes with all matches', () => {
+      const nodeWithPrefixAndPostfix = new PathParamNodeRegex('id', new RegExp('([0-9]+)'), '/', 'order-');
+      const node2 = new PathParamNodeRegex('name', new RegExp('([a-zA-Z]+)'), '.html', 'customer-')
+      const ctrl = new BasicController('controller1', 'id1', 2)
+      node2.addController(ctrl);
+      nodeWithPrefixAndPostfix.addChildNode(node2)
+
+      const foundRoutes = nodeWithPrefixAndPostfix.findRoutes('order-1234/customer-NICK.html');
+      const route1 = foundRoutes.next();
+
+      expect(route1.value.node)
+      .to
+      .equal(node2)
+
+      expect(route1.value.controller)
+      .to
+      .equal(ctrl)
+
+      expect(route1.value.params.pathParams[0].paramName)
+      .to
+      .equal('id')
+
+      expect(route1.value.params.pathParams[0].paramValue)
+      .to
+      .equal('1234')
+
+      expect(route1.value.params.pathParams[1].paramName)
+      .to
+      .equal('name')
+
+      expect(route1.value.params.pathParams[1].paramValue)
+      .to
+      .equal('NICK')
+
+    })
+
   })
+
 })
