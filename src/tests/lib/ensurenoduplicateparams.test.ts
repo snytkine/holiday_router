a427b9e872d1308a33776c261c9c63cf82fc7ead
Dmitri <dmitri.snytkine@adp.com>
2019-12-29 12:10:06 -0500

    Fixed unit test in ensurenoduplicateparams

--- src/tests/lib/ensurenoduplicateparams.test.ts
+++ src/tests/lib/ensurenoduplicateparams.test.ts
@@ -2,6 +2,7 @@ import { PathParamNode } from '../../nodes'
 import { ensureNoDuplicatePathParams } from '../../utils'
 import { expect } from 'chai';
 import { RouterErrorCode } from '../../errors'
+import { PARENT_NODE } from '../../interfaces';
 
 
 describe('#duplicateparamscheck', () => {
@@ -13,13 +14,20 @@ describe('#duplicateparamscheck', () => {
     node1.addChildNode(node2);
     node2.addChildNode(node3);
 
+    /**
+     * addChildNode nodes NOT add PARENT_NODE prop to added node
+     * per eslint (should not modify passed object)
+     * must setup PARANT_NODE props manually here
+     */
+    node2[PARENT_NODE] = node1;
+    node3[PARENT_NODE] = node2;
+
     it('#Must throw error when any of the parent nodes has same paramName', () => {
 
       try {
         ensureNoDuplicatePathParams(node3, 'id');
         throw new Error('ensureNoDuplicatePathParams must throw error for non-unique paramName "id"')
       } catch(e){
-        console.error(e.message)
         expect(e.code).to.equal(RouterErrorCode.NON_UNIQUE_PARAM)
         expect(e.message).to.equal(`URI params must be unique. Non-unique param "id" found in node=PathParamNode::id::'order-'::'.html'`)
       }
@@ -31,15 +39,14 @@ describe('#duplicateparamscheck', () => {
         ensureNoDuplicatePathParams(node3, 'person');
         throw new Error('ensureNoDuplicatePathParams must throw error for non-unique paramName "id"')
       } catch(e){
-        console.error(e.message)
         expect(e.code).to.equal(RouterErrorCode.NON_UNIQUE_PARAM)
         expect(e.message).to.equal(`URI params must be unique. Non-unique param "person" found in node=PathParamNode::person::''::'/'`)
       }
-    })
+    });
 
     it('#Must not throw if paramName is unique for all parents', () => {
       expect(ensureNoDuplicatePathParams(node3, 'mode')).to.not.throw
     })
 
   })
-})
+});
