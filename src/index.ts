05b6fd72934325767b4e18e2d7d05c85fb712b38
Dmitri <dmitri.snytkine@adp.com>
2020-01-02 07:15:14 -0500

    Added more exports to index.ts in odrer to export more types in npm module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './interfaces';
 export { default as Router } from './router';
 export { default as HttpRouter } from './httprouter';
+export * from './lib';
+export * from './errors';
